ace8cd1fe3b15c3aaab1fba609f7f4a0141f009f
Kim Burgestrand <kim@elabs.se>
2014-04-07 15:46:23 +0200

    Move keychain finder to Keychain.rb

--- lib/mellon.rb
+++ lib/mellon.rb
@@ -2,36 +2,9 @@ require "mellon/version"
 require "mellon/keychain"
 
 module Mellon
-  KEYCHAIN_REGXP = /"(.+)"/
+  KEYCHAIN_REGEXP = /"(.+)"/
 
   class << self
-    def keychain(name)
-      keychain = keychains.find { |keychain| keychain === name }
-
-      if keychain.nil?
-        crash "Could not find keychain “#{name}” in #{keychains.map(&:name).join(", ")}"
-      end
-
-      keychain
-    end
-
-    def default_keychain
-      Keychain.new(sh("security", "default-keychain")[KEYCHAIN_REGXP, 1])
-    end
-
-    def keychains
-      sh("security", "list-keychains").scan(KEYCHAIN_REGXP).map do |(keychain)|
-        Keychain.new(keychain)
-      end
-    end
-
-    private
-
-    def crash(message)
-      $stderr.puts "[ERROR] #{message}"
-      exit false
-    end
-
     def sh(*command)
       output, stderr, status = Open3.capture3(*command)
 
@@ -42,7 +15,8 @@ module Mellon
         stderr = "<no output>" if stderr.empty?
         error_string << "  " << stderr.chomp
 
-        Mellon.crash(error_string)
+        $stderr.puts "[ERROR] #{message}"
+        exit false
       end
 
       output.chomp
--- lib/mellon/keychain.rb
+++ lib/mellon/keychain.rb
@@ -1,5 +1,28 @@
 module Mellon
   class Keychain
+    class << self
+      def find(name)
+        keychain = list.find { |keychain| keychain === name }
+
+        if keychain.nil?
+          raise KeyError, "Could not find keychain “#{name}” in #{list.map(&:name).join(", ")}"
+        end
+
+        keychain
+      end
+
+      def default
+        keychain_path = Mellon.sh("security", "default-keychain")[KEYCHAIN_REGEXP, 1]
+        Keychain.new(keychain_path)
+      end
+
+      def list
+        Mellon.sh("security", "list-keychains").scan(KEYCHAIN_REGEXP).map do |(keychain_path)|
+          Keychain.new(keychain_path)
+        end
+      end
+    end
+
     def initialize(path)
       @path = path
     end
@@ -11,7 +34,8 @@ module Mellon
     end
 
     def ===(name)
-      path =~ Regexp.new(name, Regexp::IGNORECASE)
+      quoted = Regexp.quote(name)
+      path =~ Regexp.new(quoted, Regexp::IGNORECASE)
     end
   end
 end
