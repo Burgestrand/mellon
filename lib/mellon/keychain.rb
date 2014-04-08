25e0aae5c2ef1170978405ec83b751fbd7a0f329
Kim Burgestrand <kim@elabs.se>
2014-04-08 11:27:24 +0200

    Add Keychain#read

--- lib/mellon/keychain.rb
+++ lib/mellon/keychain.rb
@@ -1,3 +1,5 @@
+require "plist"
+
 module Mellon
   class Keychain
     class << self
@@ -43,11 +45,39 @@ module Mellon
       command "lock-keychain"
     end
 
+    def read(key)
+      command "find-generic-password", "-g", "-l", key do |info, password_info|
+        [parse_info(info), parse_password(password_info)]
+      end
+    end
     private
 
     def command(*command, &block)
       command += [path]
       Mellon.security *command, &block
+    end
+
+    def parse_info(info)
+      extract = lambda { |key| info[/#{key}.+=(?:<NULL>|[^"]*"(.+)")/, 1].to_s }
+      {
+        account_name: extract["acct"],
+        kind: extract["desc"],
+        type: extract["type"],
+        label: extract["0x00000007"],
+        service_name: extract["svce"],
+      }
+    end
+
+    def parse_password(password_info)
+      unpacked = password_info[/password: 0x([a-f0-9]+)/i, 1]
+      password = [unpacked].pack("H*")
+
+      parsed = Plist.parse_xml(password)
+      if parsed and parsed["NOTE"]
+        parsed["NOTE"]
+      else
+        password
+      end
     end
   end
 end
